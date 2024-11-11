42ae80267c48ceeee201adbae8492e90e05ecdb9
boriy777 <abdulboriy822@gmail.com>
2024-11-11 16:31:11 +0500

    Add files via upload

---
+++ scripe.js
@@ -0,0 +1,24 @@
+// let a = [1,2,3,4,5,6,] ;
+
+// for()
+
+let meva = ['olma', 'anor', 'uzum', 'banan', 'kiwi']
+
+let a = []
+
+let b = []
+
+console.log(meva);
+
+
+for (item of meva) {
+
+} if ( a = meva.includes('a')) {
+
+    console.log(a.push());
+    
+} else {
+
+    console.log(b.push());
+    
+}
